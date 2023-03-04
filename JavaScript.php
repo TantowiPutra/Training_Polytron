e85e890d063ae8c241952c9ac90f1d18331cc1ea
Tantowi Putra Agung Setiawan <tantowi6758@gmail.com>
2023-03-04 18:59:35 +0700

    Initialized JavaScript & Working with PHP File (Incomplete Material)

---
+++ JavaScript.php
@@ -0,0 +1,8 @@
+<!-- 
+    7	Java Script
+    7.1	Introduction to Java Script
+    7.2	Variables, operators, loops
+    7.3	Using Objects, Events
+    7.4	Common java script functions
+    7.5	Java Script Validations
+ -->
---
+++ Working with PHP & MysQL.php
@@ -0,0 +1,10 @@
+<!-- 
+    6	Working with PHP & MySQL
+    6.1	Managing Database Connections
+    6.2	Performing Queries
+    6.3	Processing Result Sets
+    6.4	Queries Which Return Data
+    6.5	Queries That Alter Data
+    6.6	Handling Errors
+    6.7	Using Ancillary Functions
+ -->
